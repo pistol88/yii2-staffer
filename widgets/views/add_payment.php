2ff6c63881d9b29e3c5a2f36ab350d90531d4201
Trishkin Vadim <halumein@gmail.com>
2016-10-28 11:44:15 +0500

    typo

--- widgets/views/add_payment.php
+++ widgets/views/add_payment.php
@@ -16,7 +16,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Струдник: <?= $staffer->name ?></h4>
+        <h4 class="modal-title">Сотрудник: <?= $staffer->name ?></h4>
       </div>
       <div class="modal-body">
         Выплаты за смену:
