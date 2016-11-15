78b5d5ac669e0cdd8e31f7db82a219d2ec08598a
Trishkin Vadim <halumein@gmail.com>
2016-11-15 19:33:01 +0500

    зарплата за сессию

--- widgets/views/worker_payments.php
+++ widgets/views/worker_payments.php
@@ -16,12 +16,28 @@ use yii\widgets\Pjax;
         'columns' => [
             ['attribute' => 'id', 'filter' => false, 'options' => ['style' => 'width: 49px;']],
             ['attribute' => 'sum', 'filter' => false, 'label' => 'Выплачено'],
+            [
+                'label' => 'Долг',
+                'content' => function($model) {
+
+                    $sessionStatistic = \Yii::$app->service->getReportBySession($model->session);
+                    return $sessionStatistic['salary'][$model->worker_id]['salary'] - $model->sum;
+                }
+            ],
             'date',
             [
                 'attribute' => 'session_id',
                 'filter' => false,
                 'content' => function($model) {
                     return "<a href=". Url::to(['/service/report/index', 'sessionId' => $model->session_id]) .">". date('d.m.Y', $model->session->start_timestamp). "</a>";
+                }
+            ],
+            [
+                'label' => 'Заработано за сессию',
+                'content' => function($model) {
+
+                    $sessionStatistic = \Yii::$app->service->getReportBySession($model->session);
+                    return$sessionStatistic['salary'][$model->worker_id]['salary'];
                 }
             ],
         ],
