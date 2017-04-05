e55a1d9eb10a027adf4fa3e86ef2ed18bbfdf709
Ilya <php@php.name>
2017-04-05 17:06:55 +0500

    user_id can be null

--- migrations/m160728_121212_Mass.php
+++ migrations/m160728_121212_Mass.php
@@ -19,7 +19,7 @@ class m160728_121212_Mass extends Migration {
             $this->createTable('{{%staffer_staffer}}', [
                 'id' => Schema::TYPE_PK . "",
                 'category_id' => Schema::TYPE_INTEGER . "(10)",
-                'user_id' => Schema::TYPE_INTEGER . "(10) NOT NULL",
+                'user_id' => Schema::TYPE_INTEGER . "(11)",
                 'name' => Schema::TYPE_STRING . "(200) NOT NULL",
                 'text' => Schema::TYPE_TEXT . " NOT NULL",
                 'pay_type' => Schema::TYPE_STRING . "(55) NOT NULL",
